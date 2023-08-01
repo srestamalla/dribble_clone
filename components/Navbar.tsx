90b9bf4b3353568356165c02dd2bd562424081f5
srestamalla <mallasresta@gmail.com>
2023-08-01 17:27:22 +0545

    footer and grafbase config

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import AuthProviders from "./AuthProviders";
 
 const Navbar = () => {
-  const session = null;
+  const session = {};
   return (
     <nav className="flexbetween navbar">
       <div className="flex-1 flexStart gap-10">
